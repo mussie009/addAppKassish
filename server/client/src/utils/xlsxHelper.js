d7d6f1272042aa96ea1cb0c8bed952d4c0b51cd8
Hans Erling Eidsvold <hans.erling@hotmail.com>
2020-12-16 08:03:57 +0100

    created own file for setting headers of the xlsx file

--- server/client/src/utils/xlsxHelper.js
+++ server/client/src/utils/xlsxHelper.js
@@ -1,7 +1,8 @@
 import XLSX from "xlsx";
+import { from_postal_code, to_postal_code, send_date } from "./columns";
 
 export const parseWorkbook = (binaryString, type) => {
-  const wb = XLSX.read(binaryString, { type });
+  const wb = XLSX.read(binaryString, { type, cellDates: true });
 
   // Get the first worksheet
   const wsname = wb.SheetNames[0];
@@ -9,10 +10,13 @@ export const parseWorkbook = (binaryString, type) => {
 
   // Convert to JSON and map to valid property name
   const data = XLSX.utils.sheet_to_json(ws).map((record) => {
+    // Format date
+    const isoDate = new Date(record[send_date]).toISOString().slice(0, 10); // blir én dag bak?
+
     return {
-      to_postal_code: record["Sendt til postnummer"],
-      from_postal_code: record["Sendt fra postnummer"],
-      send_date: new Date().toISOString().slice(0, 10), // usikker på denne...
+      from_postal_code: record[from_postal_code],
+      to_postal_code: record[to_postal_code],
+      send_date: isoDate, // usikker på denne...
       // Dimensions må også inn her, men eksisterer ikke i excel-arket
     };
   });
@@ -27,7 +31,7 @@ export const generateWorkbook = (data, fileName) => {
 
   // Create filename
   const i = fileName.lastIndexOf(".");
-  const now = new Date().toLocaleDateString('no-NO').replaceAll(".", "-");
+  const now = new Date().toLocaleDateString("no-NO").replaceAll(".", "-");
   fileName = `${fileName.substr(0, i)}-ESTIMAT-${now}.xlsx`;
 
   XLSX.writeFile(newWB, fileName);
