f44da78f7500b0973706d38ad4e393d71b7369e2
Hans Erling Eidsvold <hans.erling@hotmail.com>
2020-12-22 11:37:02 +0100

    moved instructions to component file

--- server/client/src/components/XlsxSetup.js
+++ server/client/src/components/XlsxSetup.js
@@ -1,12 +1,20 @@
 import React from "react";
-import instruction from "../resources/instructions";
 
 const XlsxSetup = (props) => {
+
+  const layout = {
+    title: "Oppsett",
+    guidelines: [
+      `Første kolonne må inneholde postnummer for hvor pakken er sendt fra`,
+      `Andre kolonne må inneholde postnummer for hvor pakken er sendt til`
+    ],
+  };
+
   return (
     <div className="m-4">
-      <h1>{instruction.title}</h1>
+      <h1>{layout.title}</h1>
       <ul>
-        {instruction.instructions.map((value, index) => {
+        {layout.guidelines.map((value, index) => {
           return <li key={index}>{value}</li>;
         })}
       </ul>
--- server/client/src/resources/instructions.js
+++
@@ -1,9 +0,0 @@
-const instruction = {
-  title: "Oppsett",
-  instructions: [
-    `Første kolonne må inneholde postnummer for hvor pakken er sendt fra`,
-    `Andre kolonne må inneholde postnummer for hvor pakken er sendt til`
-  ],
-};
-
-export default instruction;
